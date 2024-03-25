e59be89b70c2854d054466f6ebe497ab17a30e50
mahmoodfoysal <foysalcse033@gmail.com>
2024-03-26 00:34:47 +0600

    poster and popular get and post api created

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
 
-
+app.use(cors())
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster0.kdlxq0n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -24,6 +26,50 @@ async function run() {
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+        // code write here 
+        const database = client.db("burger-week");
+        const posterCollection = database.collection("poster-image-text");
+        const popularProductCollection = database.collection("popular-product")
+
+        // all post api write here 
+
+        // poster upload api 
+        app.post('/poster', async(req, res) => {
+            const newPoster = req.body;
+            console.log(newPoster);
+            const result = await posterCollection.insertOne(newPoster);
+            res.send(result);
+        });
+
+        // popular post upload api 
+        app.post('/popularProduct', async(req, res) => {
+            const popularProduct = req.body;
+            console.log(popularProduct);
+            const result = await popularProductCollection.insertOne(popularProduct);
+            res.send(result);
+        });
+
+
+
+        // all get post write here 
+
+        // get api for poster 
+        app.get('/poster', async(req, res) => {
+            const getPoster = posterCollection.find();
+            const result = await getPoster.toArray();
+            res.send(result);
+        });
+
+        app.get('/popularProduct', async(req, res) => {
+            const getPopularProduct = popularProductCollection.find();
+            const result = await getPopularProduct.toArray();
+            res.send(result);
+        })
+
+
+
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
